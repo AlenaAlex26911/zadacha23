6a95b4a60e1bbc410e86742bbb7911583f65e8db
Alena Alex <alena26911@rambler.ru>
2022-08-15 19:57:43 +0300

    zadacha 1

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿/*Задача 23
+
+Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125
+*/
+
+Console.WriteLine("Введите число N:");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+int start = 1;
+while (start <= numberN)
+{
+    Console.WriteLine(Math.Pow(start,3));
+    start++;
+}
